83fd40e739ff6d78157f5722d61336a9ae84d1ab
Joydeep Mukherjee <joydeepm02@gmail.com>
2024-02-03 16:13:18 -0500

    Basic AbstractDomain implementation for Sign

--- simpl.hs
+++ simpl.hs
@@ -15,11 +15,13 @@ import Data.Maybe
 type Env a b = [(a, b)]
 
 getEnv :: (Show a, Eq a) => Env a b -> a -> Either String b
-getEnv [] var = Left $ "unknown variable: " ++ show var
-getEnv ((x,v):rest) var = if x == var then Right v else getEnv rest var
+getEnv [] k = Left $ "unknown variable: " ++ show k
+getEnv ((x, v):rest) k = if x == k then Right v else getEnv rest k
 
-putEnv :: Env a b -> a -> b -> Env a b
-putEnv env var val = (var,val):env
+putEnv :: (Eq a) => Env a b -> a -> b -> Env a b
+putEnv env k v = (k, v):(filter (\(x, _) -> x /= k) env)
+
+type IEnv a = Env Ident a
 
 
 -- Abstract syntax tree.
@@ -60,7 +62,7 @@ type Program = Command
 
 -- Concrete interpreter.
 
-type CEnv = Env Ident Int
+type CEnv = IEnv Int
 
 evalAExp :: AExp -> CEnv -> Either String Int
 evalAExp e env = case e of
@@ -208,28 +210,154 @@ instance PartialOrd Sign
     le x y = case (x, y) of
         (_, STop) -> True
         (SBot, _) -> True
+        (SPos, SPos) -> True
+        (SZero, SZero) -> True
+        (SNeg, SNeg) -> True
         (_, _)    -> False
 
 -- TODO: Can we enforce commutativity within the typeclass?
 instance Lattice Sign
   where
     join x y = case (x, y) of
-        (_, STop) -> STop
-        (STop, _) -> STop
-        (SBot, x) -> x
-        (x, SBot) -> x
-        (_, _)    -> STop
+        (x, y) | eq x y -> x
+        (_, STop)       -> STop
+        (STop, _)       -> STop
+        (SBot, x)       -> x
+        (x, SBot)       -> x
+        (_, _)          -> STop
 
     meet x y = case (x, y) of
-        (x, STop) -> x
-        (STop, x) -> x
-        (SBot, _) -> SBot
-        (_, SBot) -> SBot
-        (_, _)    -> SBot
+        (x, y) | eq x y -> x
+        (x, STop)       -> x
+        (STop, x)       -> x
+        (SBot, _)       -> SBot
+        (_, SBot)       -> SBot
+        (_, _)          -> SBot
 
+
+class Lattice a => AbstractDomain a where
+    aEvalAExp :: (Lattice a) => AExp -> IEnv a -> Either String a
+    aEvalCommand :: (Lattice a) => Command -> IEnv a -> Either String (IEnv a)
+
+instance AbstractDomain Sign where
+    aEvalAExp e env = case e of
+        ALiteral val ->
+            Right $ if val < 0 then SNeg
+                    else if val == 0 then SZero
+                    else SPos
+        Variable var ->
+            getEnv env var
+        Add e1 e2 -> do
+            a1 <- aEvalAExp e1 env
+            a2 <- aEvalAExp e2 env
+            Right $ case (a1, a2) of
+                (SBot, _)    -> SBot
+                (_, SBot)    -> SBot
+                (SZero, x)   -> x
+                (x, SZero)   -> x
+                (SPos, SPos) -> SPos
+                (_, _)       -> STop
+        Sub e1 e2 -> do
+            a1 <- aEvalAExp e1 env
+            a2 <- aEvalAExp e2 env
+            Right $ case (a1, a2) of
+                (SBot, _)  -> SBot
+                (_, SBot)  -> SBot
+                (SZero, SPos) -> SNeg
+                (SZero, SNeg) -> SPos
+                (x, SZero) -> x
+                (SNeg, SPos) -> SNeg
+                (SPos, SNeg) -> SPos
+                (_, _) -> STop
+        Mult e1 e2 -> do
+            a1 <- aEvalAExp e1 env
+            a2 <- aEvalAExp e2 env
+            Right $ case (a1, a2) of
+                (SBot, _)    -> SBot
+                (_, SBot)    -> SBot
+                (SZero, _)   -> SZero
+                (_, SZero)   -> SZero
+                (SPos, SPos) -> SPos
+                (SNeg, SNeg) -> SPos
+                (SPos, SNeg) -> SNeg
+                (SNeg, SPos) -> SNeg
+                (_, _)       -> STop
+        Div e1 e2 -> do
+            a1 <- aEvalAExp e1 env
+            a2 <- aEvalAExp e2 env
+            Right $ case (a1, a2) of
+                (SBot, _)    -> SBot
+                (_, SBot)    -> SBot
+                (SZero, _)   -> SZero
+                (_, SZero)   -> SBot  -- Division by zero!
+                (SPos, SPos) -> SPos
+                (SNeg, SNeg) -> SPos
+                (SPos, SNeg) -> SNeg
+                (SNeg, SPos) -> SNeg
+                (_, _)       -> STop
+        Mod e1 e2 -> do
+            a1 <- aEvalAExp e1 env
+            a2 <- aEvalAExp e2 env
+            Right $ case (a1, a2) of
+                (SBot, _)    -> SBot
+                (_, SBot)    -> SBot
+                (SZero, _)   -> SZero
+                (_, SZero)   -> SBot  -- Division by zero!
+                (SPos, SPos) -> SPos
+                (SNeg, SNeg) -> SPos
+                (SPos, SNeg) -> SNeg  -- TODO: Revisit these semantics.
+                (SNeg, SPos) -> SNeg  -- TODO: Revisit these semantics.
+                (_, _)       -> STop
+
+    aEvalCommand c env = case c of
+        Skip ->
+            Right env
+        Seq c1 c2 -> do
+            env'  <- aEvalCommand c1 env
+            env'' <- aEvalCommand c2 env'
+            Right env''
+        If _ c1 c2 -> do
+            env1'  <- aEvalCommand c1 env
+            env2'  <- aEvalCommand c1 env
+            Right $ joinEnv env1' env2'
+        While _ _ ->
+            undefined
+        Assign var e -> do
+            a <- aEvalAExp e env
+            Right $ putEnv env var a
+        Print _ ->
+            Right env
+      where
+        joinEnv :: (Lattice a) => IEnv a -> IEnv a -> IEnv a
+        joinEnv env1 env2 = foldr (joinMapping env1) [] env2
+
+        joinMapping :: (Lattice a) => IEnv a -> (Ident, a) -> IEnv a -> IEnv a
+        joinMapping env1 (var, aval) acc =
+            let joined = case getEnv env1 var of
+                              Left _ -> aval
+                              Right aval1 -> Main.join aval1 aval
+            in (var, joined):acc
+
+signEvalProgram :: Program -> IO (Maybe (IEnv Sign))
+signEvalProgram p = do
+    let env' = aEvalCommand p ([] :: IEnv Sign)
+    return $ case env' of
+        Left _    -> Nothing
+        Right env -> Just env
 
 main :: IO ()
 main = do
+    let commands = [ Assign "x" (ALiteral 0)
+                   , If (Lt (Variable "x") (ALiteral 0))
+                        (Assign "x" (Mult (Variable "x") (ALiteral (-1))))
+                        (Skip)
+                   ]
+    let p = foldr (Seq) Skip commands
+    env <- signEvalProgram p
+    when (isJust env) $ putStrLn $ show $ fromJust env
+
+main2 :: IO ()
+main2 = do
     -- Hypothetical syntax:
     --
     -- x := 42;
